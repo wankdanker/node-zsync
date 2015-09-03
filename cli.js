73aedd02d94fe09225cb163eb75294d7ece84696
Dan VerWeire <dverweire@gmail.com>
2015-09-03 11:27:09 -0400

    Use debug module, fix remote stdout gather, bug fixes

--- cli.js
+++ cli.js
@@ -3,36 +3,46 @@ var zsync = require('./lib/zsync');
 var prog = require('commander');
 var slice = Function.prototype.call.bind(Array.prototype.slice);
 
-prog.command('list [dataset]')
+prog.command('list')
 	.description('list file systems')
 	.option('-u, --user [user]', 'remote ssh user')
 	.option('-k, --key [key]', 'path to ssh private key')
 	.option('-r, --remote [remote]', 'remote server')
 	.option('-t, --type [type]', 'filter file system types')
 	.option('-f, --format [format]', 'output format (json?)')
-	.option('-n, --name [name]', 'fs/vol name')
-	.option('-R, --recursive [recursive]', 'recursive fs lookup')
+	.option('-n, --name [dataset]', 'fs/vol name')
+	.option('-R, --recursive', 'recursive fs lookup')
+	.option('-e, --exclude [glob]', 'exclude datasets by glob, comma separated')
+	.option('-g, --glob [glob]', 'dataset-glob search glob')
 	.action(list)
 
-prog.command('send <source-dataset> <destination-dataset> <destination-host>')
+prog.command('send')
 	.description( 'send a local filesystem/volume to a remote server at remote filesystem/volume')
 	.option('-u, --user [user]', 'remote ssh user')
 	.option('-k, --key [key]', 'path to ssh private key')
 	.option('-r, --remote [remote]', 'remote server')
 	.option('-t, --type [type]', 'filter file system types')
 	.option('-f, --format [format]', 'output format (json?)')
-	.option('-n, --name [name]', 'fs/vol name')
+	.option('-n, --name [name]', 'source-dataset, eg: pool/vol1, pool')
+	.option('-d, --destination [name]', 'destination-base, eg: pool2/virtual-disks, pool2')
+	.option('-R, --recursive', 'Send all fileystems/volumes in source-dataset')
+	.option('-v, --verbose', 'verbose output')
+	.option('-e, --exclude [glob]', 'exclude datasets by glob, comma separated')
+	.option('-g, --glob [glob]', 'dataset-glob search glob')
+	.option('-F, --force', 'force receive (may cause rollback)')
 	.action(send)
 
-prog.command('receive')
+prog.command('receive [dataset]')
 	.description( 'receive a dataset via stdin')
 	.option('-u, --user [user]', 'remote ssh user')
 	.option('-k, --key [key]', 'path to ssh private key')
 	.option('-r, --remote [remote]', 'remote server')
 	.option('-t, --type [type]', 'filter file system types')
 	.option('-f, --format [format]', 'output format (json?)')
+	.option('-F, --force', 'force receive (may cause rollback)')
 	.option('-n, --name [name]', 'fs/vol name')
 	.option('-d, --dataset [dataset]', 'fs/vol name')
+	.option('-v, --verbose', 'verbose output')
 	.action(receive)
 
 prog.parse(process.argv);
@@ -41,46 +51,62 @@ if (!process.argv.slice(2).length) {
 	prog.outputHelp();
 }
 
-function list(args, opts) {
-	opts = parseOpts(opts);
+function list() {
+	var opts = parseOpts(arguments[arguments.length - 1]);
 
 	opts.command = 'list';
 
-	if (args) {
-		opts.name = args;
+	if (opts.exclude) {
+		opts.exclude = opts.exclude.split(',');
 	}
 
 	run(opts, function (err, list) {
-		console.log(arguments);
+		if (err) {
+			console.error(err.message);
+		}
+
+		list.forEach(function (dataset) {
+			console.log('%s', dataset.name);
+		});
 	});
 }
 
-function send(dataset, remoteDataset, remoteHost, opts) {
-	opts = parseOpts(opts);
+function send() {
+	var opts = parseOpts(arguments[arguments.length - 1]);
 
-	opts.command = 'send';
-	opts.dataset = dataset;
-	opts.remoteDataset = remoteDataset;
-	opts.remote = remoteHost;
+	opts.command = 'sendGlob';
+	opts.remoteDataset = opts.remoteDataset || opts.destination;
+
+	if (opts.exclude) {
+		opts.exclude = opts.exclude.split(',');
+	}
 
 	run(opts, function (err, result) {
-		console.log(err);
-		console.log(result);
+		if (err) {
+			console.log(err.message);
+		}
+
+		console.log('done');
 	});
 }
 
-function receive(opts) {
-	console.error('running receive');
-	opts = parseOpts(opts);
+function receive(dataset) {
+	var opts = parseOpts(arguments[arguments.length - 1]);
 	
 	opts.command = 'receive';
-	//opts.dataset = dataset;
+	opts.dataset = opts.dataset || dataset;
 	opts.stream = process.stdin;
 
 	run(opts, function (err, result) {
+		if (err) {
+			console.error(err.message);
+
+			return process.exit(1);
+		}
 		console.error('receive ended');
 		console.error(err);
 		console.error(result);
+
 	});
 }
 
@@ -107,7 +133,7 @@ function run(opts, cb) {
 	var format = opts.format;
 
 	if (opts.remote) {
-		opts.format = opts.format || 'json';
+		opts.format = opts.hasOwnProperty('format') ? opts.format : 'json';
 	}
 
 	fn = zsync[opts.command];
